9f7883f034ffc1d7064af14435744190771d7747
André Storhaug <andr3.storhaug+code@gmail.com>
2018-07-27 20:10:15 +0200

    Add initial base setup for providing SIGMA question import/export functionality

---
+++ lang/en/qformat_sigma.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component qformat_sigma.
+ *
+ * @package    qformat
+ * @subpackage sigma
+ * @author     André Storhaug <andr3.storhaug+code@gmail.com>
+ * @copyright  2018 NTNU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'SIGMA conversion';
+$string['pluginname_help'] = 'This is a format for converting SIGMA questions into STACK questions and vice versa.';
+$string['pluginname_link'] = 'qformat/sigma';
+$string['privacy:metadata'] = 'The SIGMA question format plugin does not store any personal data.';
